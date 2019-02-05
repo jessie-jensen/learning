8fb3a6085d06cee5c260382405516d5de4948519
joshua-s-jensen <joshua.s.jensen@gmail.com>
2019-02-04 23:44:07 -0800

    hands on reinforcement learning ch2

---
+++ hands_on_reinforcement_learning_with_python/README.md
@@ -0,0 +1,7 @@
+#### _My follow along code for:_
+
+## Hands on Reinforcement Learning with Python
+
+#### by Sudharsan Ravichandiran
+
+<img src="https://images-na.ssl-images-amazon.com/images/I/51KWa9yXgSL._SX260_.jpg">
---
+++ hands_on_reinforcement_learning_with_python/ch2/bot_duskdrive.py
@@ -0,0 +1,14 @@
+import gym
+import universe
+
+import random
+
+
+env = gym.make('flashgames.DuskDrive-v0')
+env.configure(remotes=1)
+observation_n = env.reset()
+
+while True:
+  action_n = [[('KeyEvent', 'ArrowUp', True)] for ob in observation_n]  # your agent here
+  observation_n, reward_n, done_n, info = env.step(action_n)
+  env.render()
